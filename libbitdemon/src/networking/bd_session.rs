8a16466f65862d00f51c8e2c682019868885b697
NefariousTechSupport <64808609+NefariousTechSupport@users.noreply.github.com>
2026-07-01 17:44:05 +0100

    Implement client->server decryption
    server->client coming soon

--- libbitdemon/src/networking/bd_session.rs
+++ libbitdemon/src/networking/bd_session.rs
@@ -9,6 +9,11 @@ pub struct BdSession {
     pub id: SessionId,
     authentication: Option<SessionAuthentication>,
     stream: BufReader<TcpStream>,
+    client_window: u32,
+    client_random: u64,
+    server_random: u64,
+    client_to_server_key: [u8; 0x10],
+    server_to_client_key: [u8; 0x10],
 }
 
 impl io::Read for BdSession {
@@ -35,6 +40,11 @@ impl BdSession {
             id: 0,
             authentication: None,
             stream: reader,
+            client_window: 0,
+            client_random: 0,
+            server_random: 0,
+            client_to_server_key: [0u8; 0x10],
+            server_to_client_key: [0u8; 0x10],
         }
     }
 
@@ -49,5 +59,45 @@ impl BdSession {
     pub fn set_authentication(&mut self, authentication: SessionAuthentication) {
         debug_assert!(self.authentication.is_none());
         self.authentication = Some(authentication);
+    }
+
+    pub fn client_window(&self) -> u32 {
+        self.client_window
+    }
+
+    pub fn set_client_window(&mut self, client_window: u32) {
+        self.client_window = client_window;
+    }
+
+    pub fn client_random(&self) -> u64 {
+        self.client_random
+    }
+
+    pub fn set_client_random(&mut self, client_random: u64) {
+        self.client_random = client_random;
+    }
+
+    pub fn server_random(&self) -> u64 {
+        self.server_random
+    }
+
+    pub fn set_server_random(&mut self, server_random: u64) {
+        self.server_random = server_random;
+    }
+
+    pub fn client_to_server_key(&self) -> &[u8; 0x10] {
+        &self.client_to_server_key
+    }
+
+    pub fn set_client_to_server_key(&mut self, client_to_server_key: &[u8; 0x10]) {
+        self.client_to_server_key.copy_from_slice(client_to_server_key);
+    }
+
+    pub fn server_to_client_key(&self) -> &[u8; 0x10] {
+        &self.server_to_client_key
+    }
+
+    pub fn set_server_to_client_key(&mut self, server_to_client_key: &[u8; 0x10]) {
+        self.server_to_client_key.copy_from_slice(server_to_client_key);
     }
 }
--- libbitdemon/src/networking/bd_socket.rs
+++ libbitdemon/src/networking/bd_socket.rs
@@ -1,17 +1,29 @@
+use crate::messaging::StreamMode;
 use crate::messaging::bd_message::BdMessage;
+use crate::messaging::bd_writer::BdWriter;
 use crate::networking::bd_session::BdSession;
 use crate::networking::session_manager::SessionManager;
+use aes::cipher::block_padding::{NoPadding, Padding};
+use aes::{Aes128, Aes128Dec};
+use aes::cipher::{BlockCipherDecrypt, BlockModeDecrypt, KeyIvInit, SetIvState};
 use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use cbc::Decryptor;
+use hmac::{Hmac, KeyInit, Mac};
 use log::{debug, error, info};
+use num_traits::ToPrimitive;
+use rand::random;
+use sha1::{Digest, Sha1};
 use snafu::{ensure, Snafu};
 use std::error::Error;
-use std::io::{ErrorKind, Read};
+use std::io::{ErrorKind, Read, Write};
 use std::net::TcpListener;
 use std::sync::Arc;
 use std::thread::JoinHandle;
 use std::{io, thread};
 
 const MAX_MESSAGE_SIZE: u32 = 0x4000000;
+type HmacSha1 = Hmac<Sha1>;
+type Aes128CbcDec = cbc::Decryptor<aes::Aes128>;
 
 #[derive(Debug, Snafu)]
 enum BdSocketError {
@@ -125,17 +137,132 @@ impl BdSocket {
                         let available_buffer_size = session.read_u32::<LittleEndian>()?;
                         debug!("Buffer available: {available_buffer_size}");
                     }
+                    210 => {
+                        let the_number_210 = session.read_u32::<LittleEndian>()?;
+                        let client_window = session.read_u32::<LittleEndian>()?;
+                        let client_random = session.read_u64::<LittleEndian>()?;
+                        session.set_client_window(client_window);
+                        session.set_client_random(client_random);
+                        session.set_server_random(random::<u64>());
+                        debug!("210 number is: {the_number_210}");
+                        debug!("client window is: {}", session.client_window());
+                        debug!("client random is: {}", session.client_random());
+                        debug!("server random is: {}", session.server_random());
+
+                        session.write_u32::<LittleEndian>(0x16)?;
+                        session.write_u8(0xAB)?;
+                        session.write_u8(0x81)?; // command type
+                        session.write_u32::<LittleEndian>(210)?;
+                        session.write_u64::<LittleEndian>(session.id)?;
+                        session.write_u64::<LittleEndian>(session.server_random())?;
+                    }
                     _ => {
                         ensure!(
                             header <= MAX_MESSAGE_SIZE,
                             MessageTooLargeSnafu { msg_size: header }
                         );
 
-                        debug!("Message with size {header}");
                         let mut msg = vec![0; header as usize];
                         session.read_exact(msg.as_mut_slice())?;
-                        let message = BdMessage::new(session, msg)?;
-                        message_handler.handle_message(session, message)?;
+                        debug!("Message with size {header} {msg:x?}");
+
+                        if msg[0] == 0xAB {
+                            let command_type = msg[1];
+                            match command_type {
+                                0x82 => {
+                                    let client_bit_buffer = &msg[2..2+0x8B];
+                                    let mut clientchalb = [0u8; 8];
+                                    clientchalb.copy_from_slice(&msg[0x8D..0x95]);
+                                    let received_clientchal = u64::from_le_bytes(clientchalb);
+                                    let mut hash_data = Vec::new();
+                                    {
+                                        let mut hash_data_writer = BdWriter::new(&mut hash_data);
+                                        hash_data_writer.set_type_checked(false);
+                                        hash_data_writer.set_mode(StreamMode::ByteMode);
+                                        // initial 210 packet
+                                        let _ = hash_data_writer.write_u32(210);
+                                        let _ = hash_data_writer.write_u32(210);
+                                        let _ = hash_data_writer.write_u32(session.client_window());
+                                        let _ = hash_data_writer.write_u64(session.client_random());
+                                        // server response to initial 210 packet
+                                        let _ = hash_data_writer.write_u32(0x16);
+                                        let _ = hash_data_writer.write_u8(0xAB);
+                                        let _ = hash_data_writer.write_u8(0x81);
+                                        let _ = hash_data_writer.write_u32(210);
+                                        let _ = hash_data_writer.write_u64(session.id);
+                                        let _ = hash_data_writer.write_u64(session.server_random());
+                                        // the packet we're responding to right now
+                                        let _ = hash_data_writer.write_u32(client_bit_buffer.len().to_u32().unwrap_or_default() + 10);
+                                        let _ = hash_data_writer.write_u8(0xAB);
+                                        let _ = hash_data_writer.write_u8(0x82);
+                                        let _ = hash_data_writer.write_bytes(&client_bit_buffer);
+                                    }
+                                    info!("0xAB 0x82 (0/4) hash data: {hash_data:x?}");
+                                    let hmac_key = Sha1::digest(hash_data);
+                                    let mut hmac_algo = HmacSha1::new_from_slice(&hmac_key).unwrap();
+                                    hmac_algo.update(&[0x42; 24]); // session key
+                                    let hmac_res = hmac_algo.finalize();
+                                    let auth_tag = hmac_res.into_bytes();
+
+                                    info!("0xAB 0x82 (1/4) auth_tag: {auth_tag:x?}");
+                                    let mut computed_clientchal = [0u8; 0x10];
+                                    let mut bddata = [0u8; 0x48];
+                                    Self::derive_key(&auth_tag, "CLIENTCHAL", &mut computed_clientchal);
+                                    Self::derive_key(&auth_tag, "BDDATA",     &mut bddata);
+
+                                    info!("0xAB 0x82 (2/4) computed client challenge: {computed_clientchal:x?}");
+                                    info!("0xAB 0x82 (3/4) client challenge received: {received_clientchal:x?}");
+                                    info!("0xAB 0x82 (4/4) bd cryptography data:      {bddata:x?}");
+
+                                    let mut computed_clientchal_copy = [0u8; 8];
+                                    computed_clientchal_copy.copy_from_slice(&computed_clientchal[0..8]);
+                                    assert_eq!(u64::from_le_bytes(computed_clientchal_copy), received_clientchal);
+
+                                    let mut hmac_key             = [0u8;   20];
+                                    let mut client_to_server_key = [0u8; 0x10];
+                                    let mut server_to_client_key = [0u8; 0x10];
+                                    hmac_key.copy_from_slice(&bddata[20..40]);
+                                    client_to_server_key.copy_from_slice(&bddata[40..56]);
+                                    server_to_client_key.copy_from_slice(&bddata[56..72]);
+                                    session.set_client_to_server_key(&client_to_server_key);
+                                    session.set_server_to_client_key(&server_to_client_key);
+
+                                    // Response
+                                    session.write_u32::<LittleEndian>(0x0A)?;
+                                    session.write_u8(0xAB)?;
+                                    session.write_u8(0x83)?; // command type
+                                    session.write_all(&computed_clientchal[8..16])?;
+                                }
+                                0x85 => {
+                                    let mut u32_buffer = [0u8; 4];
+
+                                    u32_buffer.copy_from_slice(&msg[2..6]);
+                                    let msg_count = u32::from_le_bytes(u32_buffer);
+
+                                    let mut iv = [0u8; 0x10];
+                                    iv.copy_from_slice(&msg[6..0x16]);
+
+                                    let enc_size = msg.len() - 0x1E;
+                                    let mut encrypted = vec![0u8; enc_size];
+                                    encrypted.copy_from_slice(&msg[0x16..msg.len() - 8]);
+
+                                    let mut hash = [0u8; 0x08];
+                                    hash.copy_from_slice(&msg[0x16+enc_size..msg.len()]);
+
+                                    let decrypted = Aes128CbcDec::new_from_slices(session.client_to_server_key(), &iv)?
+                                        .decrypt_padded::<NoPadding>(&mut encrypted).unwrap();
+
+                                    debug!("Decrypted Message with size {enc_size} {hash:x?} {decrypted:x?}");
+                                }
+                                _ => {
+                                    debug!("Ecountered command type {command_type} and don't know what to do");
+                                }
+                            }
+                        }
+                        else {
+                            let message = BdMessage::new(session, msg)?;
+                            message_handler.handle_message(session, message)?;
+                        }
                     }
                 }
             }
@@ -151,6 +278,39 @@ impl BdSocket {
             } else {
                 error!("Session terminated with error: {e}")
             }
+        }
+    }
+
+    fn derive_key(auth_tag: &[u8], salt: &str, out: &mut [u8]) {
+        let out_len = out.len();
+        let mut out_offset = 0;
+        let mut prev_key = [0u8; 20];
+
+        let mut iteration = 1;
+        while out_offset < out_len {
+            let mut buffer = Vec::new();
+            {
+                let mut writer = BdWriter::new(&mut buffer);
+
+                if out_offset > 0 {
+                    let _ = writer.write_bytes(&prev_key);
+                }
+
+                let _ = writer.write_bytes(&salt.as_bytes());
+
+                let _ = writer.write_u8(iteration);
+            }
+
+            let mut hmac_algo = HmacSha1::new_from_slice(&auth_tag).unwrap();
+            hmac_algo.update(&buffer);
+
+            let hmac_res = hmac_algo.finalize();
+            prev_key.copy_from_slice(&hmac_res.into_bytes());
+            let remaining_out = std::cmp::min(20, out_len - out_offset);
+            out[out_offset..out_offset+remaining_out].copy_from_slice(&prev_key[..remaining_out]);
+
+            out_offset += 20;
+            iteration += 1;
         }
     }
 }
